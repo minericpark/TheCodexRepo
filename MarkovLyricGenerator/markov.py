52c026b4021cc3f98ad4d36358f0635330102c98
detols <mpsericpark@gmail.com>
2021-04-09 15:44:56 -0400

    MarkovLyricGenerator: Lesson 4 - Generating the array of strings using the Markov model

--- MarkovLyricGenerator/markov.py
+++ MarkovLyricGenerator/markov.py
@@ -1,3 +1,4 @@
+from random import randint
 # Markov chain is basically just a probability flowchart based on frequency from one word to the next set,
 # then the next set, etc.
 class MarkovLyrics:
@@ -24,7 +25,25 @@ class MarkovLyrics:
                     next_word = words[i + 1]
                     self.chain[word] = [next_word]
 
-data = ["I am avi", "I am an engineer", "I like to code"]
-m = MarkovLyrics()
-m.populateMarkovChain(data)
-print(m.chain)
+    def generateLyrics(self, length=500):
+        n = len(self.chain)
+
+        # Select random index from 0 and max
+        start_index = randint(0, n-1)
+        keys = list(self.chain.keys())
+        # .title capitalizes first letter of word
+        current_word = keys[start_index].title()
+
+        lyrics = current_word + " "
+
+        for _ in range(length):
+            if current_word not in self.chain:
+                lyrics += "\n"
+                next_index = randint(0, n - 1)
+                current_word = keys[next_index]
+            else:
+                next_words = self.chain[current_word]
+                next_index = randint(0, len(next_words) - 1)
+                next_word = next_words[next_index]
+                lyrics += next_word + ' '
+                current_word = next_word
